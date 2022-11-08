24a38ed5778b65eb95da293266f31f6e2141b741
Daniil Lashin <danil-lashin@yandex.ru>
2022-11-08 15:50:27 +0300

    tests

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const hre = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+describe("MinterHubExtender", function () {
+  it("Should deploy", async function () {
+    const MinterHubExtender = await hre.ethers.getContractFactory("MinterHubExtender");
+    const extender = await MinterHubExtender.deploy();
+    await extender.deployed();
   });
 });
